4d6f02840573dfe133a2004a75e7b47d83844a49
skitheom <SahoKitahara@my.uopeople.edu>
2025-02-08 15:37:46 +0900

    files created

---
+++ ex02/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 15:33:04 by sakitaha          #+#    #+#             */
+/*   Updated: 2025/02/08 15:37:25 by sakitaha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Error" << std::endl;
+    return 1;
+  }
+  return 0;
+}
