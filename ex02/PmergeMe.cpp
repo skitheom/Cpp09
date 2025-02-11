4928e199bccc2cf0f416efd3abef9d2e0a8a77fa
skitheom <SahoKitahara@my.uopeople.edu>
2025-02-11 20:18:43 +0900

    template整理

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -13,8 +13,6 @@
 #include "PmergeMe.hpp"
 #include <cstdlib>
 
-// TODO: テンプレートを使った処理の共通化
-
 PmergeMe::PmergeMe() {}
 
 PmergeMe::PmergeMe(const PmergeMe &other) { (void)other; }
@@ -32,7 +30,13 @@ void PmergeMe::sort(std::vector<int> &vec) {
     std::cerr << "Error: not enough elements to be sorted\n";
     std::exit(1);
   }
-  mergeInsertionSort(vec);
+  std::vector<std::pair<int, int> > pairs;
+  std::vector<int> sorted;
+
+  pairs.reserve(vec.size() / 2);
+  sorted.reserve(vec.size());
+
+  mergeInsertionSort(vec, pairs, sorted);
 }
 
 void PmergeMe::sort(std::deque<int> &deq) {
@@ -40,66 +44,8 @@ void PmergeMe::sort(std::deque<int> &deq) {
     std::cerr << "Error: not enough elements to be sorted\n";
     std::exit(1);
   }
-  mergeInsertionSort(deq);
-}
-
-void PmergeMe::mergeInsertionSort(std::vector<int> &vec) {
-  const size_t size = vec.size();
-
-  // 1.pairを作る
-  std::vector<std::pair<int, int> > pairs;
-  int singleElement = -1;
-
-  if (size % 2 != 0) {
-    singleElement = vec.back();
-    vec.pop_back();
-  }
-  pairs.reserve(size / 2); // vectorにだけ必要
-  buildPairs(vec, pairs);
-
-  // 2. 各pairのうち、大きい方の値(Leaders)をMerge Sortする
-  mergeSortPairs(pairs, 0, pairs.size() - 1);
-
-  // 3. Jacobsthal数列に従いleaders & followersを挿入
-  std::vector<int> sorted;
-  sorted.reserve(size);
-  insertJacobsthal(pairs, sorted);
-
-  // 4.singleElementをbinaryInsertion
-  if (singleElement != -1) {
-    binaryInsertion(sorted, singleElement);
-  }
-  // 5. result を　originalのvecに入れ替える
-  // 最初から、vecを初期化->書き換えるでも良かったし、
-  // sortedの結果を別のvectorで返す設計でも良かったのかもしれない
-  // 結局sortが同一データ構造上で起こるべきかどうかという問題ではある
-  vec = sorted;
-}
-
-void PmergeMe::mergeInsertionSort(std::deque<int> &deq) {
-  const size_t size = deq.size();
-
-  // 1.pairを作る
   std::deque<std::pair<int, int> > pairs;
-  int singleElement = -1;
-
-  if (size % 2 != 0) {
-    singleElement = deq.back();
-    deq.pop_back();
-  }
-  buildPairs(deq, pairs);
-
-  // 2. 各pairのうち、大きい方の値(Leaders)をMerge Sortする
-  mergeSortPairs(pairs, 0, pairs.size() - 1);
-
-  // 3. Jacobsthal数列に従いleaders & followersを挿入
   std::deque<int> sorted;
-  insertJacobsthal(pairs, sorted);
 
-   // 4.singleElementをbinaryInsertion
-   if (singleElement != -1) {
-    binaryInsertion(sorted, singleElement);
-  }
-  // 5. result を　originalのdequeに入れ替える
-  deq = sorted;
+  mergeInsertionSort(deq, pairs, sorted);
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,15 +6,18 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 18:48:53 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/02/11 19:32:07 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/02/11 20:16:53 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#include <algorithm>
+#include <deque>
 #include <iostream>
 #include <utility>
+#include <vector>
 class PmergeMe {
 public:
   static void sort(std::vector<int> &vec);
@@ -27,18 +30,19 @@ private:
 
   PmergeMe &operator=(const PmergeMe &other);
 
-  static void mergeInsertionSort(std::vector<int> &vec);
-  static void mergeInsertionSort(std::deque<int> &deq);
-
+  /**
+   * Step 1: Build pairs from the unsorted container.
+   **/
   template <typename T, typename U>
-  static void buildPairs(T &pairs, const U &nums) {
-    if (nums.size() % 2 != 0) {
+  static void buildPairs(T &pairs, const U &unsorted) {
+    if (unsorted.size() % 2 != 0) {
       throw std::runtime_error("Error: Number of elements must be even");
     }
-    for (typename U::const_iterator it = nums.begin(); it != nums.end(); ++it) {
+    for (typename U::const_iterator it = unsorted.begin(); it != unsorted.end();
+         ++it) {
       int a = *it;
       ++it;
-      if (it == nums.end()) {
+      if (it == unsorted.end()) {
         throw std::runtime_error("Error: Unexpected end before making pair");
       }
       int b = *it;
@@ -47,8 +51,11 @@ private:
       }
       pairs.push_back(std::make_pair(a, b));
     }
-  }
+  };
 
+  /**
+   * Step 2: Perform merge sort on pairs based on the values of the leaders.
+   */
   template <typename T>
   static void mergeSortPairs(T &pairs, size_t left, size_t right) {
     if (left >= right) {
@@ -77,7 +84,6 @@ private:
     }
 
     size_t leftIndex = 0, rightIndex = 0, pairsIndex = left;
-
     while (leftIndex < sizeLeft && rightIndex < sizeRight) {
       if (leftPairs[leftIndex].first <= rightPairs[rightIndex].first) {
         pairs[pairsIndex++] = leftPairs[leftIndex++];
@@ -94,9 +100,28 @@ private:
   }
 
   /**
-   * jacobthalに基づいて、leadersはちょっとずつpushされる
-   * prevJaobより大きく、currJaboc以下のindexPosのleadersをpushする
+   * Step 3: Insert values based on the Jacobsthal sequence.
    */
+  template <typename T, typename U>
+  static void insertJacobsthal(const T &pairs, U &sorted) {
+
+    size_t pairsSize = pairs.size();
+    size_t prevJacob = 0, currJacob = 1;
+
+    sorted.push_back(pairs[0].second);
+    sorted.push_back(pairs[0].first);
+    if (pairsSize == 1) {
+      return;
+    }
+    while (prevJacob < pairsSize) {
+      insertLeaders(pairs, sorted, prevJacob, currJacob);
+      insertFollowers(pairs, sorted, prevJacob, currJacob);
+      size_t nextJacob = currJacob + 2 * prevJacob;
+      prevJacob = currJacob;
+      currJacob = nextJacob;
+    }
+  }
+
   template <typename T, typename U>
   static void insertLeaders(const T &pairs, U &sorted, size_t prevJacob,
                             size_t currJacob) {
@@ -105,11 +130,6 @@ private:
     }
   }
 
-  /**
-   * jacobthalのindexPosにあるfollower値を、先にsortedに挿入
-   * 残りの部分は、indexのより小さいものから順番にbinaryInsertion
-   * prevJaobより大きく、currJaboc以下のindexPosのfollowersが対象
-   */
   template <typename T, typename U>
   static void insertFollowers(const T &pairs, U &sorted, size_t prevJacob,
                               size_t currJacob) {
@@ -121,55 +141,32 @@ private:
     }
   }
 
+  /**
+   * Step 4: Insert values using binary search (binary insertion).
+   */
   template <typename U> static void binaryInsertion(U &sorted, int value) {
     typename U::iterator it;
     it = std::lower_bound(sorted.begin(), sorted.end(), value);
     sorted.insert(it, value);
   }
 
+  /**
+   * Step 5: Execute the Merge Insertion Sort algorithm.
+   */
   template <typename T, typename U>
-  static void insertJacobsthal(const T &pairs, U &sorted) {
-
-    size_t pairsSize = pairs.size();
-    size_t prevJacob = 0, currJacob = 1;
-
-    sorted.push_back(pairs[0].second); // pairs[0]のfollowerは最小値
-    sorted.push_back(pairs[0].first);  // index処理をfolower/leaderで揃えるため
-    if (pairsSize == 1) {
-      return;
+  static void mergeInsertionSort(U &unsorted, T &pairs, U &sorted) {
+    int singleElement = -1;
+    if (size % 2 != 0) {
+      singleElement = unsorted.back();
+      unsorted.pop_back();
     }
-    // prevJacobがpairsSize未満 = 未ソート値あり
-    while (prevJacob < pairsSize) {
-      // prevJacobsthal値より大きく、currJacob以下のindexPosのleadersを挿入
-      insertLeaders(pairs, sorted, prevJacob, currJacob);
-      // prevJacobshtal値より大きく、currJacob以下のindexPosのfollowersを挿入
-      insertFollowers(pairs, sorted, prevJacob, currJacob);
-      // 次のJacobsthal numberを用意しておく
-      size_t nextJacob = currJacob + 2 * prevJacob;
-      prevJacob = currJacob;
-      currJacob = nextJacob;
+    buildPairs(vec, pairs);
+    mergeSortPairs(pairs, 0, pairs.size() - 1);
+    insertJacobsthal(pairs, sorted);
+    if (singleElement != -1) {
+      binaryInsertion(sorted, singleElement);
     }
+    unsorted = sorted;
   }
-
-  template <typename T, typename U>
-  static void mergeInsertionSort(T &pairs, U &unsorted, U &sorted) {
-
-      // 1.pairを作る
-
-
-  }
-
-  /*
-  Jaobsthal number 参考値
-  初期値: prev: 0, curr: 1
-  i = 0: prev: 1, curr: 1 この呼び出しが無意味？ 1より大きく1以下は存在しない
-  i = 1: prev: 1, curr: 3
-  i = 2: prev: 3, curr: 5
-  i = 3: prev: 5, curr: 11
-  i = 4: prev: 11, curr: 21
-  i = 5: prev: 21, curr: 43
-  i = 6: prev: 43, curr: 85
-  */
 };
-
 #endif // PMERGEME_HPP
