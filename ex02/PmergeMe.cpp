6d9d9acbbbd14a2018a431b204aa8a9189ca8046
skitheom <SahoKitahara@my.uopeople.edu>
2025-02-11 16:52:04 +0900

    mergePairs done

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,11 +6,14 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 15:33:08 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/02/10 15:25:50 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/02/11 01:0 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <cstdlib>
+
+// TODO: テンプレートを使った処理の共通化
 
 PmergeMe::PmergeMe() {}
 
@@ -24,12 +27,79 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
   return *this;
 }
 
-void PmergeMe::sort(std::vector<int> &vec) { (void)vec; }
+void PmergeMe::sort(std::vector<int> &vec) {
+  if (vec.size() < 2) {
+    std::cerr << "Error: not enough elements to be sorted\n";
+    std::exit(1);
+  }
+  mergeInsertionSort(vec);
+}
 
-void PmergeMe::sort(std::deque<int> &deq) { (void)deq; }
+void PmergeMe::sort(std::deque<int> &deq) {
+  if (deq.size() < 2) {
+    std::cerr << "Error: not enough elements to be sorted\n";
+    std::exit(1);
+  }
+  mergeInsertionSort(deq);
+}
 
-void PmergeMe::mergeInsertionSort(std::vector<int> &vec) { (void)vec; }
+/*
 
-void PmergeMe::mergeInsertionSort(std::deque<int> &deq) { (void)deq; }
 
-// TODO: テンプレートを使った処理の共通化
+int main() {
+    std::vector<std::pair<int, int> > pairs; // ✅ C++98 の正しい書き方
+
+    // std::make_pair を使ってペアを追加
+    pairs.push_back(std::make_pair(3, 5));
+    pairs.push_back(std::make_pair(7, 9));
+
+    // ペアの内容を表示
+    for (size_t i = 0; i < pairs.size(); ++i) {
+        std::cout << "Pair " << i << ": (" << pairs[i].first << ", " <<
+pairs[i].second << ")" << std::endl;
+    }
+
+    return 0;
+}
+*/
+
+void PmergeMe::mergeInsertionSort(std::vector<int> &vec) {
+  const size_t size = vec.size();
+
+  // 1.pairを作る
+  std::vector<std::pair<int, int> > pairs;
+  std::vector<int> singleElement;
+
+  if (size % 2 != 0) {
+    singleElement.push_back(vec.back());
+    vec.pop_back();
+  }
+  pairs.reserve(size / 2); // vectorにだけ必要
+  buildPairs(vec, pairs);
+
+  // 2. 各pairのうち、大きい方の値(Leaders)をMerge Sortする
+  mergeSortPairs(pairs, 0, pairs.size() - 1);
+
+  // 3. 最初だけ特殊で、あとはJacobsthal数列に従いfollowersを挿入
+  std::vector<int> sorted;
+  sorted.reserve(size);
+  sorted.push_back(pairs[0].second); // secondが最小値
+  // Leadersはsortedにすべていれる
+  for (size_t i = 0; i < pairs.size(); ++i) {
+    sorted.push_back(pairs[i].first);
+  }
+
+  // [...]
+}
+
+void PmergeMe::mergeInsertionSort(std::deque<int> &deq) {
+  std::deque<std::pair<int, int> > pairs;
+  std::deque<int> singleElement;
+
+  if (deq.size() % 2 != 0) {
+    singleElement.push_back(deq.back());
+    deq.pop_back();
+  }
+  buildPairs(deq, pairs);
+  // ソート処理...
+}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 18:48:53 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/02/11 01:25:51 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/02/11 03:18:58 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@ private:
   static void mergeInsertionSort(std::deque<int> &deq);
 
   template <typename T, typename U>
-  static void buildPairs(const T &nums, U &pairs) {
+  static void buildPairs(T &pairs, const U &nums) {
     if (nums.size() % 2 != 0) {
       throw std::runtime_error("Error: Number of elements must be even");
     }
-    for (typename T::const_iterator it = nums.begin(); it != nums.end(); ++it) {
+    for (typename U::const_iterator it = nums.begin(); it != nums.end(); ++it) {
       int a = *it;
       ++it;
       if (it == nums.end()) {
@@ -75,10 +75,11 @@ private:
     for (size_t j = 0; j < sizeRight; ++j) {
       rightPairs.push_back(pairs[mid + 1 + j]);
     }
+
     size_t leftIndex = 0, rightIndex = 0, pairsIndex = left;
+
     while (leftIndex < sizeLeft && rightIndex < sizeRight) {
-      if (leftPairs[leftIndex].first <=
-          rightPairs[rightIndex].first) {
+      if (leftPairs[leftIndex].first <= rightPairs[rightIndex].first) {
         pairs[pairsIndex++] = leftPairs[leftIndex++];
       } else {
         pairs[pairsIndex++] = rightPairs[rightIndex++];
@@ -90,6 +91,45 @@ private:
     while (rightIndex < sizeRight) {
       pairs[pairsIndex++] = rightPairs[rightIndex++];
     }
+  }
+
+  template <typename T, typename U>
+  static void insertFollowers(const T &pairs, U &sorted) {
+
+    size_t indexToInsert, indexPairs, lastPos, realIndex;
+    size_t pairCount = 0;
+    size_t nthJacob = 0;
+    size_t indexJacob = 0;
+  }
+
+  size_t jacobsthal(size_t n) {
+
+    std::vector<int> jacobNums;
+
+    jacobNums.reserve(n + 1);
+    jacobNums[0] = 0;
+    jacobNums[1] = 1;
+    for (size_t i = 2; i <= n; ++i) {
+      jacobNums[i] = jacobNums[i - 1] + 2 * jacobNums[i - 2];
+    }
+    return jacobNums[n];
+  }
+  template <typename T, typename U>
+  static void jacobsthalInsertion(const T &pairs, U &sorted) {
+    size_t prev1 = 0, prev2 = 1;
+
+    if (n == 0) {
+      return prev1;
+    }
+    if (n == 1) {
+      return prev2;
+    }
+    for (size_t i = 2; i <= n; ++i) {
+      size_t curr = prev2 + 2 * prev1;
+      prev1 = prev2;
+      prev2 = curr;
+    }
+    return prev2;
   }
 };
 
